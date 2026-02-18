cdc6bab3abd45917f13b28aa9db892c171ea9126
AnuragRajput-cyber <andrenotech123@gmail.com>
2026-02-18 16:39:22 +0530

    [Anurag] Added:feature/UC2-FeetAndInchesMeasurementEquality and rename the method names inside feet inner class in QuanityMeasurementApp

---
+++ QuantityMeasurementApp/src/com/apps/QuantityMeasurmentAppTest.java
@@ -0,0 +1,64 @@
+package com.apps;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.apps.QuantityMeasurmentApp.Feet;
+import com.apps.QuantityMeasurmentApp.Inches;
+
+public class QuantityMeasurmentAppTest {
+
+    @Test
+    void testInchesEquality_SameValue() {
+        assertEquals(new Inches(2.5), new Inches(2.5));
+    }
+
+    @Test
+    void testInchesEquality_DifferentValue() {
+        assertNotEquals(new Inches(5.0), new Inches(7.0));
+    }
+
+    @Test
+    void testInchesEquality_NullComparison() {
+        assertNotEquals(new Inches(5.2), null);
+    }
+
+    @Test
+    void testInchesEquality_NonNumericInput() {
+        assertNotEquals(new Inches(4.1), "Other String");
+    }
+
+    @Test
+    void testInchesEquality_SameReference() {
+        Inches f1=new Inches(8.3);
+        assertEquals(f1,f1);
+    }
+
+    // FEET TESTS
+
+    @Test
+    void testFeetEquality_SameValue() {
+        assertEquals(new Feet(2.5), new Feet(2.5));
+    }
+
+    @Test
+    void testFeetEquality_DifferentValue() {
+        assertNotEquals(new Feet(5.0), new Feet(7.0));
+    }
+
+    @Test
+    void testFeetEquality_NullComparison() {
+        assertNotEquals(new Feet(5.2), null);
+    }
+
+    @Test
+    void testFeetEquality_NonNumericInput() {
+        assertNotEquals(new Feet(4.1), "Other String");
+    }
+
+    @Test
+    void testFeetEquality_SameReference() {
+        Feet f1=new Feet(8.3);
+        assertEquals(f1,f1);
+    }
+}
